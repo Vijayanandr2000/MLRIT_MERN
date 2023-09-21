2e275fc7e81aace073bb9819cf0d9643c8613511
vijayanand <vijayanandr2000@gmail.com>
2023-09-21 19:48:50 +0530

    code added

---
+++ Backend - TODO/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Todo = require('./Model/todoModel');
+
+const app = express();
+
+const PORT = 8080;
+
+mongoose.connect('mongodb+srv://vijayanandr2000:dY5pzzc0XEWGYxwe@cluster0.bp7sq77.mongodb.net/?retryWrites=true&w=majority')
+
+const db = mongoose.connection;
+
+db.on('error', () => {
+    console.log("Error while DB connection")
+})
+
+db.once('open', async () => {
+
+    await Todo.create({
+        task: "Task 2"
+    })
+
+    console.log("DB is connected")
+})
+
+app.get('/', (req, res) => {
+    res.send("Hello, world!");
+})
+
+app.listen(PORT, () => {
+    console.log("Server is started")
+});
+
+
+// http://localhost:8080
+
